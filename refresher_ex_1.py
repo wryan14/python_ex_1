b0627bc7076625ae990c1f64bccbe26d308266af
Chris Harding <charding@iastate.edu>
2020-05-25 12:22:10 -0500

    initial

---
+++ refresher_ex_1.py
@@ -0,0 +1,67 @@
+# Python refresher exercises
+# part 1 
+# May 25, 2020
+
+# This set of exercises uses topics from refreshers 1 and 2
+# variables, basic data types and flow control
+# I have just given you descriptions of tasks and what the result should look like.
+# My solution is directly below, but with a visual buffer so you don't see it accidentally
+
+# You should have gotten this file by forking the python_ex_1 project from my github
+# repo: 
+
+# How to work the exercises:
+# Use a debugger to set a break point at the print() at the top of the solution part so you
+# don't accidentally run beyond it and see the solution. This is the "end" breakpoint
+# Set another "start" breakpoint at the print() that shows the start of each part
+# write you code between those break points
+# to run your code, start the debugger (will stop at the "start" breakpoint) and
+# hit step or continue. Once you have your bug free solution, remove the start and
+# end breakpoints and move on to the next part. Here's an example:
+
+
+# part 1
+# This is just a simple example to demonstrate how the start and end
+# breakpoint system works
+# Task: get the user's name with input() and print out hello <name>
+print("start of part 1") # set breakpoint here
+# your code here
+
+
+print("end of 1") # set breakpoint here 
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# solution 1
+name = input("What's your name?")
+print("Hello", name)
+'''
+
+# Before we really start, a couple more things:
+# - your solution may be different or even better than what I showed
+# - Please get in the habit of documenting your code as you go along
+#   You don't need to state the obvious but you should point out anything 
+#   that could be interesting later.
+# - After you've finished the code for your part
