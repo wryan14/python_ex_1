b9bf2056cc17abc1c755c5af7658e4fcc551ba02
Chris Harding <charding@iastate.edu>
2020-05-25 14:26:45 -0500

    typo

--- refresher_ex_1.py
+++ refresher_ex_1.py
@@ -84,7 +84,8 @@ print("Hello", name)
 # this will run your file and put the output into results.txt.
 # on Canvas hand in:
 # - your version of this .py file
-# -
+# - your results.txt
+# - tell me the URL of your forked repo 
 
 # part 1
 # task:  L = [0, [], [1,2,3,4], [[5],[6,7]], [8,9,10]]
